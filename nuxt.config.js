26fe741c97c4d9f64988dce2f9488fcb12ed095e
sefakrb <sefa799@gmail.com>
2023-03-11 12:07:27 +0300

    add ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  ssr: false,
+  // ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Cultrav',
